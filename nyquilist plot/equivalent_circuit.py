Equivalent circuit calculation fix

nyquilist plot/equivalent_circuit.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import least_squares
+from scipy.optimize import curve_fit
 
 def equivalent_circuit_fit(frequency, impedance):
     """
@@ -30,10 +31,6 @@ def equivalent_circuit_fit(frequency, impedance):
     R_s, R_p, C_p = result.x
     return R_s, R_p, C_p
 
-
-import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
 
 def sort_by_frequency(freqs, Z):
     idx = np.argsort(freqs)[::-1]  # High to low freq
@@ -95,15 +92,17 @@ def standard_circuit_model(freqs, R_s, R_p, C_p):
     return R_s + Z_parallel
 
 def fit_standard_circuit(freqs, Z):
-    def real_part(freqs, R_s, R_p, C_p):
-        return standard_circuit_model(freqs, R_s, R_p, C_p).real
-    def imag_part(freqs, R_s, R_p, C_p):
-        return standard_circuit_model(freqs, R_s, R_p, C_p).imag
+    freqs_extended = np.logspace(-2, 6, 52)
+
+    def real_part(freqs_extended, R_s, R_p, C_p):
+        return standard_circuit_model(freqs_extended, R_s, R_p, C_p).real
+    def imag_part(freqs_extended, R_s, R_p, C_p):
+        return standard_circuit_model(freqs_extended, R_s, R_p, C_p).imag
 
     Z_real = Z.real
     Z_imag = Z.imag
     p0 = [0.2, 0.5, 1e-6]
-    bounds = ([0, 0, 1e-9], [10, 10, 1e-3])
+    bounds = ([0.1, 0.1, 1e-9], [1, 1, 1e-1])
     popt_real, _ = curve_fit(real_part, freqs, Z_real, p0=p0, bounds=bounds)
     popt_imag, _ = curve_fit(imag_part, freqs, Z_imag, p0=p0, bounds=bounds)
 
@@ -131,7 +130,9 @@ def fit_cpe_circuit(freqs, Z):
     popt_real, _ = curve_fit(real_part, freqs, Z_real, p0=p0, bounds=bounds)
     popt_imag, _ = curve_fit(imag_part, freqs, Z_imag, p0=p0, bounds=bounds)
 
-    return (popt_real + popt_imag) / 2
+    R_s, R_p, C_p, n = (popt_real + popt_imag) / 2
+
+    return R_s, R_p, C_p
 
 # ----- STEP 4: Automatic Logic -----
 def auto_fit_eis(freqs, Z):

nyquilist plot/nyquilist.py
@@ -1,47 +1,20 @@
 import numpy as np
 import plotly.graph_objects as go
-from scipy.fft import fft, fftfreq
 from single_points import load_data_single_freq, extract_impedance_points
-from preprocess import load_data, preprocess_data, low_pass_filter
-from nyquilist_plot import make_nyquist_plot
-
-
-def time_to_frequency(time, signal):
-    """Convert time-domain signal to frequency domain using FFT."""
-    dt = time[1] - time[0]
-    N = len(time)
-
-    #signal = signal - np.mean(signal)
-
-    # Apply Hann window to reduce spectral leakage
-    window = np.hanning(N)
-    signal_windowed = signal * window
-
-    freq = fftfreq(N, dt)
-    #S_f = fft(signal_windowed)
-    S_f = fft(signal_windowed)/ N
-
-    positive_idx = freq > 0.1
-    #print(freq[positive_idx], S_f[positive_idx])
-    return freq[positive_idx], S_f[positive_idx]
 
 
 def main():
-    filename = "data/data29.txt"
+    filename = "data/data27.txt"
 
     time, current, voltage, freq = load_data_single_freq(filename)
     Z, f, fitted_impedance = extract_impedance_points(time, current, voltage, freq)
 
-    # Fit circuit model
-    #R_s, R_p, C_p = equivalent_circuit_fit(f, Z)
-    #print(f"Fitted Circuit Parameters:\nR_s = {R_s:.2f} Ω, R_p = {R_p:.2f} Ω, C_p = {C_p:.2e} F")
-    #fitted_impedance = R_s + 1 / (1/R_p + 1j * 2 * np.pi * f * C_p)
 
     fig = go.Figure()
     # Add the measured data
-    fig.add_trace(go.Scatter(x=Z.real, y=-Z.imag, mode='markers', name='Measured Data'))
+    fig.add_trace(go.Scatter(x=Z.real, y=-Z.imag, mode='lines+markers', name='Measured Data'))
     # Add the fitted impedance
-    fig.add_trace(go.Scatter(x=fitted_impedance.real, y=-fitted_impedance.imag, mode='lines', name='Fitted Circuit'))
+    fig.add_trace(go.Scatter(x=fitted_impedance.real, y=-fitted_impedance.imag, mode='lines+markers', name='Fitted Circuit'))
     # Customize the layout
     fig.update_layout(
         title='Nyquist Plot',
@@ -55,133 +28,4 @@ def main():
     fig.show()
 
 
-    """
-
-    
-    # Load and preprocess data
-    time, current, voltage = load_data(filename)
-    time, voltage, current, sample_rate = preprocess_data(time, voltage, current)
-
-    voltage = low_pass_filter(voltage, cutoff_freq=50, sample_rate=sample_rate)
-    current = low_pass_filter(current, cutoff_freq=50, sample_rate=sample_rate)
-
-
-    fig = go.Figure()
-    # Add the measured data
-
-    # First trace - voltage (left y-axis)
-    fig.add_trace(go.Scatter(
-        x=time,
-        y=voltage,
-        mode='markers',
-        name='Voltage',
-        yaxis='y1'
-    ))
-
-    # Second trace - current (right y-axis)
-    fig.add_trace(go.Scatter(
-        x=time,
-        y=current,
-        mode='markers',
-        name='Current',
-        marker=dict(color='orange'),
-        yaxis='y2'
-    ))
-
-    # Update layout to add a second y-axis
-    fig.update_layout(
-        title='Nyquist Plot',
-        xaxis=dict(title='Time'),
-        yaxis=dict(
-            title='Voltage',
-            #titlefont=dict(color='blue'),
-            tickfont=dict(color='blue')
-        ),
-        yaxis2=dict(
-            title='Current',
-            #titlefont=dict(color='orange'),
-            tickfont=dict(color='orange'),
-            overlaying='y',
-            side='right'
-        ),
-        template='plotly_dark',
-        showlegend=True,
-        dragmode='zoom'
-    )
-    # Show the plot
-    fig.show()
-
-    resistance = []
-
-    # Transform to frequency domain
-    alg_type = 2    #best no.2
-
-    if alg_type == 1:
-        for i, x in enumerate(voltage):
-            resistance.append((voltage[0] - x) / (current[i]))
-        frequency, impedance = time_to_frequency(time, resistance)
-    
-    if alg_type == 2:
-        for x in voltage:
-            resistance = (voltage[0] - x) / current
-        frequency, impedance = time_to_frequency(time, resistance)
-
-    if alg_type == 3:
-        resistance = voltage / current
-        frequency, impedance = time_to_frequency(time, resistance)
-
-    if alg_type == 4: 
-        frequency, V_f = time_to_frequency(time, voltage)
-        _, I_f = time_to_frequency(time, current)  # FFT of current
-        impedance = V_f / I_f # Prevent division errors
-
-
-
-    #for x in resistance: print(x)
-
-    
-    fig = go.Figure()
-
-    # Add the measured data
-    fig.add_trace(go.Scatter(x=time, y=resistance, mode='markers', name='Measured Data'))
-    # Customize the layout
-    fig.update_layout(
-        title='Nyquist Plot',
-        xaxis_title='time',
-        yaxis_title='resistance',
-        template='plotly_dark',  # You can change the theme here
-        showlegend=True,
-        dragmode='zoom'  # Enable zoom interaction
-    )
-    # Show the plot
-    # fig.show()
-
-
-
-    # Fit circuit model
-    #R_s, R_p, C_p = equivalent_circuit_fit(frequency, impedance)
-   # print(f"Fitted Circuit Parameters:\nR_s = {R_s:.2f} Ω, R_p = {R_p:.2f} Ω, C_p = {C_p:.2e} F")
-    #fitted_impedance = R_s + 1 / (1/R_p + 1j * 2 * np.pi * frequency * C_p)
-    
-    #for x in impedance: x*1000*1000j
-    fig = go.Figure()
-    # Add the measured data
-    fig.add_trace(go.Scatter(x=impedance.real, y=-impedance.imag, mode='markers', name='Measured Data'))
-    # Add the fitted impedance
-    #fig.add_trace(go.Scatter(x=fitted_impedance.real, y=fitted_impedance.imag, mode='lines', name='Fitted Circuit'))
-    # Customize the layout
-    fig.update_layout(
-        title='Nyquist Plot',
-        xaxis_title='Re(Z) [Ohms]',
-        yaxis_title='Im(Z) [Ohms]',
-        template='plotly_dark',  # You can change the theme here
-        showlegend=True,
-        dragmode='zoom'  # Enable zoom interaction
-    )
-    # Show the plot
-    fig.show()
-
-    make_nyquist_plot(filename)
-
-    """
 main()

nyquilist plot/preprocess.py
@@ -1,17 +1,7 @@
 import numpy as np
 from scipy.signal import butter, filtfilt, resample
+from scipy.fft import fft, fftfreq
 
-
-def load_data(filename):
-    """Load data from text file: timestamp (us), current (mA), voltage (mV)."""
-    skip_row = 100
-    data = np.loadtxt(filename, delimiter=',', skiprows=0)
-
-    timestamps = data[:, 0] * 1e-3  # Convert miliseconds to seconds
-    current = data[:, 2] * 1e-3  # Convert mA to A
-    voltage = data[:, 3] * 1e-3  # Convert mV to V
-
-    return timestamps, current, voltage
 
 def preprocess_data(time, voltage, current):
     """Interpolates data for uniform sampling and removes DC offset."""
@@ -33,3 +23,4 @@ def low_pass_filter(signal, cutoff_freq, sample_rate, order=2):
     normal_cutoff = min(cutoff_freq / nyquist, 0.99)
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     return filtfilt(b, a, signal)
+

nyquilist plot/single_points.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 from scipy.fft import fft, fftfreq
 from preprocess import preprocess_data, low_pass_filter
 from equivalent_circuit import equivalent_circuit_fit, auto_fit_eis
+from plots import input_plot
 
 
 
@@ -21,97 +22,55 @@ def extract_impedance_points(time, current, voltage, freq):
     unique_freqs = np.unique(freq)
     for f in unique_freqs:
 
-       # if f > 10:
-        #    continue
+        if f < 1.5:
 
-        # Mask rows for this frequency
-        mask = freq == f
-        indices = np.where(mask)[0]
+            # Mask rows for this frequency
+            mask = freq == f
+            indices = np.where(mask)[0]
 
-        # Exclude first and last row
-        if len(indices) <= 30:
-            continue
-        idx_range = indices[10:-1]
+            # Exclude first and last row
+            if len(indices) <= 30:
+                continue
+            idx_range = indices[10:-1]
 
-        t_seg = time[idx_range]
-        i_seg = current[idx_range] - np.mean(current[idx_range])
-        v_seg = - voltage[idx_range] + np.mean(voltage[idx_range])#+ voltage[0]
+            t_seg = time[idx_range]
+            i_seg = current[idx_range] - np.mean(current[idx_range])
+            v_seg = - voltage[idx_range] + np.mean(voltage[idx_range])#+ voltage[0]
 
-        t_seg, v_seg, i_seg, sample_rate = preprocess_data(t_seg, v_seg, i_seg)
+            t_seg, v_seg, i_seg, sample_rate = preprocess_data(t_seg, v_seg, i_seg)
 
-        v_seg = low_pass_filter(v_seg, cutoff_freq=10*f, sample_rate=sample_rate)
-        i_seg = low_pass_filter(i_seg, cutoff_freq=10*f, sample_rate=sample_rate)
+            v_seg = low_pass_filter(v_seg, cutoff_freq=10*f, sample_rate=sample_rate)
+            i_seg = low_pass_filter(i_seg, cutoff_freq=10*f, sample_rate=sample_rate)
 
+            #input_plot(t_seg, v_seg, i_seg, f)
+          
+            # Window to reduce leakage
+            window = np.hanning(len(t_seg))
+            i_seg *= window
+            v_seg *= window
 
-        fig = go.Figure()
-        # Add the measured data
+            # FFT
+            I_fft = fft(i_seg)
+            V_fft = fft(v_seg)
+            dt = np.mean(np.diff(t_seg))
+            freqs_fft = fftfreq(len(t_seg), dt)
 
-        # First trace - voltage (left y-axis)
-        fig.add_trace(go.Scatter(
-            x=t_seg,
-            y=v_seg,
-            mode='markers',
-            name='Voltage',
-            yaxis='y1'
-        ))
+            # Get index of closest FFT bin to target frequency
+            target_idx = np.argmin(np.abs(freqs_fft - f))
+            Z = V_fft[target_idx] / I_fft[target_idx]
 
-        # Second trace - current (right y-axis)
-        fig.add_trace(go.Scatter(
-            x=t_seg,
-            y=i_seg,
-            mode='markers',
-            name='Current',
-            marker=dict(color='orange'),
-            yaxis='y2'
-        ))
-            # Update layout to add a second y-axis
-        fig.update_layout(
-            title='Nyquist Plot ' + str(f),
-            xaxis=dict(title='Time'),
-            yaxis=dict(
-                title='Voltage',
-                #titlefont=dict(color='blue'),
-                tickfont=dict(color='white')
-            ),
-            yaxis2=dict(
-                title='Current',
-                #titlefont=dict(color='orange'),
-                tickfont=dict(color='orange'),
-                overlaying='y',
-                side='right'
-            ),
-            template='plotly_dark',
-            showlegend=True,
-            dragmode='zoom'
-        )
-        # Show the plot
-        #fig.show()
-
-
-        # Window to reduce leakage
-        window = np.hanning(len(t_seg))
-        i_seg *= window
-        v_seg *= window
-
-        # FFT
-        I_fft = fft(i_seg)
-        V_fft = fft(v_seg)
-        dt = np.mean(np.diff(t_seg))
-        freqs_fft = fftfreq(len(t_seg), dt)
-
-        # Get index of closest FFT bin to target frequency
-        target_idx = np.argmin(np.abs(freqs_fft - f))
-        Z = V_fft[target_idx] / I_fft[target_idx]
-
-        Z_list.append(Z)
-        freq_list.append(f)
+            Z_list.append(Z)
+            freq_list.append(f)
 
     impedance = np.array(Z_list)
 
     freqs = np.array(freq_list)
 
-    R_s, R_p, C_p, _ = auto_fit_eis(freqs, impedance)
+   # R_s, R_p, C_p, _ = auto_fit_eis(freqs, impedance)
+    R_s, R_p, C_p = equivalent_circuit_fit(freqs, impedance)
     print(f"Fitted Circuit Parameters:\nR_s = {R_s:.2f} Ω, R_p = {R_p:.2f} Ω, C_p = {C_p:.2e} F")
+
+
     fitted_impedance = R_s + 1 / (1/R_p + 1j * 2 * np.pi * freqs * C_p)
 
     return np.array(Z_list), np.array(freq_list), fitted_impedance